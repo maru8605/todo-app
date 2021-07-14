5e1dd335b3a234f528f215173a6ffff5f57f28e1
maru8605 <maru8605@hotmail.com>
2021-07-14 17:37:23 -0300

    inicio de proyecto

---
+++ src/component/Main.js
@@ -0,0 +1,22 @@
+
+
+const Main = () => {
+    return (
+        <section className='main'>
+            <div>
+                <h1>TO DO</h1>
+                <i></i>
+            </div>
+            <input/>
+            <div>
+                <input/>
+                <div>
+                    {/* links */}
+                </div>
+            </div>
+            <div><h3>footer</h3></div>
+        </section>
+    )
+}
+
+export default Main
